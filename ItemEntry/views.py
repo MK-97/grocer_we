0228ea55b3a0273ea027ed9d38d1e46daf821688
marsilinou97 <marsilinou.97@gmail.com>
2019-04-21 00:17:25 -0700

    10% done

--- ItemEntry/views.py
+++ ItemEntry/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-
-
-    return 0
+    names = ["Mina", "Mark", "Bianca", "Marsilinou"]
+    items = {"Rice": 4.25, "Milk": 7.37, "Egg": 5.99}
+    # map = {}
+    return render(request, 'index.html', {"names": names, "items": items})
--- NameEntry/views.py
+++ NameEntry/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    names = ["Mina", "Mark", "Bianca", "Marsilinou"]
+    items = {"Rice": 4.25, "Milk": 7.37, "Egg": 5.99}
+    # map = {}
+    return render(request, 'index.html', {"names": names})
