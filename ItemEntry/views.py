0c0d9f4620cd259b4d81e689dde66608cac31fcb
Mark Nguyen <mkqnn97@gmail.com>
2019-06-07 23:56:44 -0700

    ajax in progress

--- ItemEntry/views.py
+++ ItemEntry/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse, JsonResponse
+import json
 
 
 def index(request):
 
-    return render(request, 'test.html') 
+    return render(request, 'test.html')
+
+def get_data(request):
+
+    print(request.method)
+    print(type(request.GET))
+    print(request.GET)
+    if request.method == 'POST':
+        print("hello")
+        JSON_load = json.loads(request.GET)
+        print(JSON_load)
+
+    return HttpResponse('hleo')
